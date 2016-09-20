ccdb3caa9c35e393617ba3eb3d9778d1cb850d80
Stian Søvik <stian.s@gmail.com>
2016-09-20 22:02:36 +0200

    Add clear function, separate load and commit functions for local VD table

--- persist.lua
+++ persist.lua
@@ -1,37 +1,46 @@
 -- Persistent storage for VDs, heavily inspired from Krikoff's Sonos VD
 local persist = { 
-	persist_global_var = "lua_persist",
-	load = function(this)
-      local persist_table_str = fibaro:getGlobalValue(this.persist_global_var)
-      if string.len(persist_table_str) > 0 and persist_table_str ~= "NaN" then
-        local persist_table = json.decode(persist_table_str)
-        if persist_table and type(persist_table) == "table" then 
-          return persist_table
-        else
-          fibaro:debug("Error loading persistent table: " .. (persistent_table_str or "(empty)"))
-        end
+  persist_global_var = "lua_persist",
+  load = function(this)
+    local persist_table_str = fibaro:getGlobalValue(this.persist_global_var)
+    if string.len(persist_table_str) > 0 and persist_table_str ~= "NaN" then
+      local persist_table = json.decode(persist_table_str)
+      if persist_table and type(persist_table) == "table" then 
+        return persist_table
       else
-        fibaro:debug("Empty table string, initializing.")
-        this:init()
+        fibaro:debug("Error loading persistent table: " .. (persistent_table_str or "(empty)"))
       end
-    end,
-	set = function(this, set_table)
-      local persist_table = this:load()
-      local vd_id = "id_" .. fibaro:getSelfId()
-      if persist_table[vd_id] then 
+    else
+      fibaro:debug("Empty table string, initializing.")
+      this:init()
+    end
+  end,
+  getId = function(this)
+    return "id_" .. fibaro:getSelfId()
+  end,
+  loadLocalTable = function(this)
+    return this:load()[this:getId()]
+  end,
+  commitLocalTable = function(this, commit_table)
+    local persist_table = this:load()
+    persist_table[this:getId()] = commit_table
+    fibaro:setGlobal(this.persist_global_var, json.encode(persist_table))
+  end,
+  set = function(this, set_table)
+      local persist_table = this:loadLocalTable()
+      if persist_table then
         for key, value in pairs(set_table) do
-          persist_table[vd_id][key] = value
+          persist_table[key] = value
         end
       else 
-        persist_table[vd_id] = set_table
+        persist_table = set_table
       end;
-      fibaro:setGlobal(this.persist_global_var, json.encode(persist_table))
+      this:commitLocalTable(persist_table)
     end,
-	get = function(this, get_key)
-      local persist_table = this:load()
-      local vd_id = "id_" .. fibaro:getSelfId()
-      if persist_table[vd_id] and type(persist_table[vd_id]) == "table" then 
-        for key, value in pairs(persist_table[vd_id]) do
+  get = function(this, get_key)
+      local persist_table = this:loadLocalTable()
+      if persist_table and type(persist_table) == "table" then 
+        for key, value in pairs(persist_table) do
           if tostring(key) == tostring(get_key or "") then
             return value
           end
@@ -39,15 +48,21 @@ local persist = {
       end;
       return nil
     end,
-    reset = function(this)
-      local persist_table = this:load()
-      local vd_id = "id_" .. fibaro:getSelfId()
-      if persist_table[vd_id] then
-      	persist_table[vd_id] = nil
-      end
-      fibaro:setGlobal(this.persist_global_var, json.encode(persist_table)) 
-    end,
-  	init = function(this)
-      fibaro:setGlobal(this.persist_global_var, json.encode({}))
-	end
+  clear = function(this, clear_key)
+    fibaro:debug("clearing " .. clear_key)
+    local persist_table = this:loadLocalTable()
+    persist_table[clear_key] = nil
+    this:commitLocalTable(persist_table)
+  end,
+  reset = function(this)
+    local persist_table = this:load()
+    local vd_id = "id_" .. fibaro:getSelfId()
+    if persist_table[vd_id] then
+      persist_table[vd_id] = nil
+    end
+    fibaro:setGlobal(this.persist_global_var, json.encode(persist_table)) 
+  end,
+  init = function(this)
+    fibaro:setGlobal(this.persist_global_var, json.encode({}))
+  end,
 }
